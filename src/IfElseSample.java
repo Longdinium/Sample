7d8593822c4226e9d10942c288cc18471c064ed2
Longdinium <rondobell0303@gmail.com>
2023-11-26 09:58:31 +0900

    クラスの移動

---
+++ src/IfElseSample.java
@@ -0,0 +1,19 @@
+
+public class IfElseSample {
+
+    public static void main(String[] args) {
+        int score = -10; // 成績値
+
+        // scoreが60以上ならば
+        if (score >= 60) {
+            // 合格です！と表示
+            System.out.println("合格です！");
+        } else {
+            // 再テストですと表示
+            System.out.println("再テストです");
+
+        }
+
+    }
+
+}
