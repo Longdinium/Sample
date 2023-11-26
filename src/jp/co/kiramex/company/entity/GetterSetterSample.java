532395b0f0cb506c2ed427d46108f65151b7fb28
Longdinium <rondobell0303@gmail.com>
2023-11-26 10:23:41 +0900

    jp.co.kiramex.company.entityパッケージの作成

---
+++ src/jp/co/kiramex/company/entity/GetterSetterSample.java
@@ -0,0 +1,11 @@
+package jp.co.kiramex.company.entity;
+
+public class GetterSetterSample {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.employeeName = "大島";
+
+    }
+
+}
