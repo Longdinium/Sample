a988c0735aea760cf97a275bd5adafe2ec9762db
Longdinium <rondobell0303@gmail.com>
2023-12-10 17:35:39 +0900

    抽象クラスDivision、その中に抽象メソッドgreetingの定義

--- src/jp/co/kiramex/company/entity/Division.java
+++ src/jp/co/kiramex/company/entity/Division.java
@@ -1,7 +1,20 @@
 package jp.co.kiramex.company.entity;
 
 public abstract class Division {
+    // フィールドの定義
+    private String employeeName; // 社員名
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
     // 抽象メソッドgreetingの定義
+        // 抽象メソッドは、スーパークラス側ではメソッドの詳細を記述せず、
+        // サブクラスのほうで実際の処理内容を記述するために使います。
     public abstract void greeting();
 
 }
